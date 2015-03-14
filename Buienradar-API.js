671a55bd1da603696f0f0056dbbc88cb6a21c0d9
freekwebdesign <freek@freekwebdesign.nl>
2015-03-14 23:43:23 +0100

    Small improvements
    - Correct hour calculation
    - Added rain_found variable

--- Buienradar-API.js
+++ Buienradar-API.js
@@ -4,35 +4,48 @@
 
 var lat = 52.221537;
 var lon = 6.893662;
-var found;
+var rain_found = 0;
+var i;
 
-var request = require('request');
-request('http://gps.buienradar.nl/getrr.php?lat=' + lat + '&lon=' + lon, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var array = body.split('\r\n'); //split into seperate items
+function will_it_rain() {
+    var request = require('request');
+    //request('http://gps.buienradar.nl/getrr.php?lat=' + lat + '&lon=' + lon, function (error, response, body) {
+    request('http://boelders.nl/uni/test/test-api.html', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var array = body.split('\r\n'); //split into seperate items
 
-    for (i = 1; i < 7; i++) { //get the coming 30 min (ignore first 2 items)
-    	var array2 = array[i].split('|'); //split rain and time
+        for (i = 1; i < 7; i++) { //get the coming 30 min (ignore first 2 items)
+        	var array2 = array[i].split('|'); //split rain and time
 
-    	if (array2[0] > 0 && found != 1){ //if rain is more then 0 and not already found
-    		var rainTime = array2[1];
-			var rainMinute = rainTime.substr(rainTime.indexOf(":") + 1);
+        	if (array2[0] > 0 && rain_found != 1){ //if rain is more then 0 and not already found
+        		var rainTime = array2[1];
+    			var rainMinute = parseInt(rainTime.substr(rainTime.indexOf(":") + 1));
+                var rainHours = parseInt(rainTime.substr(rainTime.indexOf(":") - 2, 2));
+                var d = new Date();
+                var hours = d.getHours();
+                var currentMinute = d.getMinutes();
 
-    		var d = new Date();
-			var hours = d.getHours();
-			var currentMinute = d.getMinutes();
+                if (hours == rainHours) {
+                    var difMinute = rainMinute - currentMinute;
+                }
+                else {
+                    var difMinute = 60 - currentMinute + rainMinute;
+                }
 
-			var difMinute = rainMinute - currentMinute; //does not work yet if there is a change in hour involved!
+    			var rain_found = 1;
 
-			found = 1;
-    	}
-    }
+                console.log(rain_found);
+        	}
+        }
+      }
+    })
+    setTimeout(will_it_rain, 1000);
+};
 
-    if (found == 1) {
-		console.log("It's going to rain within the next " + difMinute +" min");
+will_it_rain(); // call function
+
+if (rain_found == 1) {
+        console.log("It's going to rain within the next " + difMinute +" min");
     } else {
-    	console.log("No rain expected within the next 30 min")
+        console.log("No rain expected within the next 30 min")
     }
-
-  }
-})
